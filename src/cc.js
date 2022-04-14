394938a1be35b6df3f4aa07e4e7f67b701aff39f
lalitha <20b01a1260@svecw.edu.in>
2022-04-14 19:11:53 +0530

    this is my commit

---
+++ src/cc.js
@@ -0,0 +1,42 @@
+import react, { Component} from "react";
+
+class CC extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+        };
+    }
+    // changeCountAdd = () => {
+        
+            // this.setState({ count: this.state.count + 1 });
+        
+    // };
+
+    // changeCountMinus = () => {
+            
+    //         this.setState({ count: this.state.count - 1});
+        
+    // };
+    changeCountAdd = () => {
+            setInterval(()=>{
+        this.setState({ count: this.state.count + 1});
+            },1000);
+};
+     changeCountMinus = () => {
+    setInterval(()=>{
+          this.setState({ count: this.state.count - 1});
+    },1000);
+};
+    render() {
+        return (
+            <div>
+                <h3>COUNT = {this.state.count}</h3>
+                <button onClick={this.changeCountAdd}>+</button>
+                &nbsp;&nbsp;
+                <button onClick={this.changeCountMinus}>-</button>
+            </div>
+        );
+    }
+}
+export default CC;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import react, { Component} from "react";
+import ReactDOM from "react-dom";
+import CC from "./cc";
+function w(){
+  return<h1></h1>;
+}
+ReactDOM.render(
+    <div>
+        <center>
+            <CC />
+        </center>
+    </div>,
+    document.getElementById("root")  );
